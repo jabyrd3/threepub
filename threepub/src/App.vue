c574e9547ec3a0a0752226388f7573a668ef2f5f
jordan byrd <jabyrd3@gmail.com>
2016-02-24 18:04:55 -0500

    init commit, working with vue, webpack, epub.js

---
+++ threepub/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <render-book></render-book>
+  </div>
+</template>
+
+<script>
+// import rsvp from 'rsvp/dist/rsvp.min.js'
+// import localforage from 'epub.js/build/libs/localforage.min.js'
+// window.RSVP = rsvp
+// window.localforage = localforage
+import RenderBook from './components/RenderBook'
+export default {
+  components: {
+    RenderBook
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ threepub/src/components/RenderBook.vue
@@ -0,0 +1,56 @@
+<template>
+
+<div class="control" v-on:click="prevPage">‹</div>
+  <div class="epub" id="area">
+  </div>
+<div class="control" v-on:click="nextPage">›</div>
+</template>
+
+<script>
+import epub from 'epub.js/build/epub.js'
+import book from 'epub.js/books/alice.epub'
+let Book
+export default {
+  data () {
+    return {}
+  },
+  ready () {
+    Book = epub(book)
+    Book.renderTo('area')
+  },
+  methods: {
+    prevPage: function () {
+      Book.prevPage()
+    },
+    nextPage: function () {
+      Book.nextPage()
+    }
+  }
+}
+</script>
+<style type="text/css">
+  iframe{
+    position: fixed;
+    height: 90vh;
+    width: 90vw;
+    margin-left: 5vw;
+    margin-top: 5vw;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 999;
+  } 
+  .control{
+    position: absolute;
+    top: 0;
+    z-index: 1000;
+
+  }
+  .control:first-of-type{
+    left: 0;
+  }
+  .control:last-of-type{
+    right: 0;
+  }
+</style>
